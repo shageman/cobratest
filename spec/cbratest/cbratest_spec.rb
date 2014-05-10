ef2af8be87876e59c9fbf8d0a60b41bd6eb1f108
Stephan Hagemann <stephan.hagemann@gmail.com>
2014-05-10 17:44:32 -0600

    Integration testing this way is hard

--- spec/cbratest/cbratest_spec.rb
+++ spec/cbratest/cbratest_spec.rb
@@ -9,9 +9,30 @@ describe Cbratest do
   end
 
   it "outputs all affected components in verbose mode" do
-    start_path = File.expand_path(File.join(__FILE__, "..", "..", "..", "spec", "examples", "letters", "A"))
+    start_path = File.expand_path(File.join(__FILE__, "..", "..", "..", "spec", "examples", "letters"))
 
-    Cbratest::Runner.new(true).run(start_path)
-    #expect(@puts).to eq([])
+    Cbratest::Runner.new(true).run(File.join(start_path, 'A'))
+    expect(@puts).to eq(
+                         [
+                             "All components",
+                             [
+                                 "B    #{start_path}/B",
+                                 "C    #{start_path}/C",
+                                 "D    #{start_path}/D",
+                                 "E1   #{start_path}/E1",
+                                 "E2   #{start_path}/E2",
+                                 "F    #{start_path}/F",
+                                 "A    #{start_path}/A"
+                             ],
+                             "\nChanges since last commit",
+                             [],
+                             "\nDirectly affected components",
+                             [],
+                             "\nTransitively affected components",
+                             [],
+                             "\nTest scripts to run",
+                             []
+                         ]
+                     )
   end
 end
