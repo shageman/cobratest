ff88ffbd1172e2c9f88242472e66140769d9f702
Garett Arrowood <garettarrowood@gmail.com>
2017-04-06 07:30:44 -0400

    Update rspec stub syntax to fix verbose spec

--- spec/cobratest/cobratest_spec.rb
+++ spec/cobratest/cobratest_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Cbratest do
   before do
     @puts = []
-    Cbratest::Runner.any_instance.stub(:output) do |arg|
+    allow_any_instance_of(Cbratest::Runner).to receive(:output) do |obj, arg|
       @puts << arg
     end
   end
@@ -13,26 +13,26 @@ describe Cbratest do
 
     Cbratest::Runner.new(true).run(File.join(start_path, 'A'))
     expect(@puts).to eq(
-                         [
-                             "All components",
-                             [
-                                 "B    #{start_path}/B",
-                                 "C    #{start_path}/C",
-                                 "D    #{start_path}/D",
-                                 "E1   #{start_path}/E1",
-                                 "E2   #{start_path}/E2",
-                                 "F    #{start_path}/F",
-                                 "A    #{start_path}/A"
-                             ],
-                             "\nChanges since last commit",
-                             [],
-                             "\nDirectly affected components",
-                             [],
-                             "\nTransitively affected components",
-                             [],
-                             "\nTest scripts to run",
-                             []
-                         ]
-                     )
+      [
+        "All components",
+        [
+          "B    #{start_path}/B",
+          "C    #{start_path}/C",
+          "D    #{start_path}/D",
+          "E1   #{start_path}/E1",
+          "E2   #{start_path}/E2",
+          "F    #{start_path}/F",
+          "A    #{start_path}/A"
+        ],
+        "\nChanges since last commit",
+        [],
+        "\nDirectly affected components",
+        [],
+        "\nTransitively affected components",
+        [],
+        "\nTest scripts to run",
+        []
+      ]
+    )
   end
 end
